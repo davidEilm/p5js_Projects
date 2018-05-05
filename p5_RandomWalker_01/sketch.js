6953e1565d1574f90d245c09002d1a633e27bba5
David Eilmsteiner <d.eilmsteiner@students.htl-leonding.ac.at>
2018-05-05 17:18:26 +0200

    Made RandomWalker faster, and implemented 'gravity' towards the center

--- p5_RandomWalker_01/sketch.js
+++ p5_RandomWalker_01/sketch.js
@@ -7,6 +7,7 @@ class Coord {
 
 var lastCoord;
 var hu = 0;
+var gravity = 0.03;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -17,21 +18,20 @@ function setup() {
 }
 
 function draw() {
-	for (let i = 0; i < 50; i++) {
+	for (let i = 0; i < 2000; i++) {
 		var coord = new Coord(lastCoord.x + random(-5, 5), lastCoord.y + random(-5, 5))
 		if (coord.x > windowWidth/2) {
-			coord.x -= 0.01;
+			coord.x -= gravity;
 		} else if (coord.x < windowWidth/2) {
-			coord.x += 0.01;
+			coord.x += gravity;
 		}
 		if (coord.y > windowHeight/2) {
-			coord.y -= 0.01;
+			coord.y -= gravity;
 		} else if (coord.y < windowHeight/2) {
-			coord.y += 0.01;
+			coord.y += gravity;
 		}
-		stroke(hu,hu * 1.1, hu * 3);
+		stroke(hu,255,255);
 		line(lastCoord.x, lastCoord.y, coord.x, coord.y);
-		
 		lastCoord = coord;
 	}
 	hu += 3;
@@ -42,7 +42,9 @@ function draw() {
 
 function keyPressed() {
 	if (keyCode === 82) { //R -> Reset
+		colorMode(RGB);
 		background(55, 55, 55);
+		colorMode(HSB);
 		lastCoord = new Coord(windowWidth/2,windowHeight/2);
 	}
 }
